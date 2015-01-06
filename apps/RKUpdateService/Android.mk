a5bd5c6b9562069a606d4ccbb20a2646bdb4a936
zhanghao <zhanghao@rock-chips.com>
2015-01-06 14:25:37 +0800

    add sofia RKUpdateService & StressTest

--- apps/RKUpdateService/Android.mk
+++ apps/RKUpdateService/Android.mk
@@ -1,6 +1,8 @@
 ###############################################################################
 # RKUpdateService
 LOCAL_PATH := $(call my-dir)
+
+ifeq ($(strip $(TARGET_BOARD_HARDWARE)), rk30board)
 include $(CLEAR_VARS)
 LOCAL_MODULE := RKUpdateService
 LOCAL_MODULE_CLASS := APPS
@@ -15,3 +17,20 @@ LOCAL_SRC_FILES := $(LOCAL_MODULE).apk
 LOCAL_PREBUILT_JNI_LIBS := \
     lib/arm/librockchip_update_jni.so
 include $(BUILD_PREBUILT)
+endif
+
+ifeq ($(strip $(TARGET_BOARD_HARDWARE)), sofiaboard)
+include $(CLEAR_VARS)
+LOCAL_MODULE := RKUpdateService
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_BUILT_MODULE_STEM := package.apk
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+#LOCAL_PRIVILEGED_MODULE :=
+LOCAL_CERTIFICATE := PRESIGNED
+#LOCAL_OVERRIDES_PACKAGES := 
+LOCAL_SRC_FILES := $(LOCAL_MODULE)_sofia.apk
+#LOCAL_REQUIRED_MODULES :=
+#LOCAL_PREBUILT_JNI_LIBS := 
+include $(BUILD_PREBUILT)
+endif
--- apps/StressTest/Android.mk
+++ apps/StressTest/Android.mk
@@ -1,6 +1,9 @@
 ###############################################################################
 # StressTest
 LOCAL_PATH := $(call my-dir)
+
+ifeq ($(strip $(TARGET_BOARD_HARDWARE)), rk30board)
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := StressTest
 LOCAL_MODULE_CLASS := APPS
@@ -23,3 +26,22 @@ LOCAL_MODULE_STEM := $(LOCAL_MODULE)
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
 
+endif
+
+ifeq ($(strip $(TARGET_BOARD_HARDWARE)), sofiaboard)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := StressTest
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_BUILT_MODULE_STEM := package.apk
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+#LOCAL_OVERRIDES_PACKAGES := 
+LOCAL_SRC_FILES := $(LOCAL_MODULE)_sofia.apk
+LOCAL_REQUIRED_MODULES := \
+    getbootmode.sh
+include $(BUILD_PREBUILT)
+
+endif
