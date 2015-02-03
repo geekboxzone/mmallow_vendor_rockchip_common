64056b5b107bee1a38909ee2c8d7ad7de7a098fa
zxl <zhuangxl@rock-chips.com>
2015-02-03 19:21:15 +0800

    RK3368 GPU version: L0.02

--- gpu/G6110.mk
+++ gpu/G6110.mk
@@ -1,25 +1,11 @@
 # lib
 PRODUCT_COPY_FILES += \
     vendor/rockchip/common/gpu/libG6110/lib/egl/egl.cfg:system/lib/egl/egl.cfg \
-    vendor/rockchip/common/gpu/libG6110/lib/modules/dc_fbdev.ko:system/lib/modules/dc_fbdev.ko \
     vendor/rockchip/common/gpu/libG6110/lib/modules/pvrsrvkm.ko:system/lib/modules/pvrsrvkm.ko 
 
 # vendor/bin
 PRODUCT_COPY_FILES += \
-    vendor/rockchip/common/gpu/libG6110/vendor/bin/pvrsrvctl:system/vendor/bin/pvrsrvctl \
-    vendor/rockchip/common/gpu/libG6110/vendor/bin/framebuffer_test:system/vendor/bin/framebuffer_test \
-    vendor/rockchip/common/gpu/libG6110/vendor/bin/hwperfbin2jsont:system/vendor/bin/hwperfbin2jsont \
-    vendor/rockchip/common/gpu/libG6110/vendor/bin/pvrdebug:system/vendor/bin/pvrdebug \
-    vendor/rockchip/common/gpu/libG6110/vendor/bin/pvrhwperf:system/vendor/bin/pvrhwperf \
-    vendor/rockchip/common/gpu/libG6110/vendor/bin/pvrlogdump:system/vendor/bin/pvrlogdump \
-    vendor/rockchip/common/gpu/libG6110/vendor/bin/pvrtld:system/vendor/bin/pvrtld \
-    vendor/rockchip/common/gpu/libG6110/vendor/bin/rgx_blit_test:system/vendor/bin/rgx_blit_test \
-    vendor/rockchip/common/gpu/libG6110/vendor/bin/rgx_blitsize_test:system/vendor/bin/rgx_blitsize_test \
-    vendor/rockchip/common/gpu/libG6110/vendor/bin/rgx_compute_test:system/vendor/bin/rgx_compute_test \
-    vendor/rockchip/common/gpu/libG6110/vendor/bin/rgx_triangle_test:system/vendor/bin/rgx_triangle_test \
-    vendor/rockchip/common/gpu/libG6110/vendor/bin/rgx_twiddling_test:system/vendor/bin/rgx_twiddling_test \
-    vendor/rockchip/common/gpu/libG6110/vendor/bin/testwrap:system/vendor/bin/testwrap \
-    vendor/rockchip/common/gpu/libG6110/vendor/bin/testwrap32:system/vendor/bin/testwrap32
+    vendor/rockchip/common/gpu/libG6110/vendor/bin/pvrsrvctl:system/vendor/bin/pvrsrvctl
 
 # vendor/lib
 PRODUCT_COPY_FILES += \
@@ -27,7 +13,6 @@ PRODUCT_COPY_FILES += \
     vendor/rockchip/common/gpu/libG6110/vendor/lib/egl/libGLESv1_CM_POWERVR_ROGUE.so:system/vendor/lib/egl/libGLESv1_CM_POWERVR_ROGUE.so \
     vendor/rockchip/common/gpu/libG6110/vendor/lib/egl/libGLESv2_POWERVR_ROGUE.so:system/vendor/lib/egl/libGLESv2_POWERVR_ROGUE.so \
     vendor/rockchip/common/gpu/libG6110/vendor/lib/hw/gralloc.rk3368.so:system/vendor/lib/hw/gralloc.rk3368.so \
-    vendor/rockchip/common/gpu/libG6110/vendor/lib/hw/hwcomposer.rk3368.so:system/vendor/lib/hw/hwcomposer.rk3368.so \
     vendor/rockchip/common/gpu/libG6110/vendor/lib/libcreatesurface.so:system/vendor/lib/libcreatesurface.so \
     vendor/rockchip/common/gpu/libG6110/vendor/lib/libglslcompiler.so:system/vendor/lib/libglslcompiler.so \
     vendor/rockchip/common/gpu/libG6110/vendor/lib/libIMGegl.so:system/vendor/lib/libIMGegl.so \
@@ -42,7 +27,6 @@ PRODUCT_COPY_FILES += \
     vendor/rockchip/common/gpu/libG6110/vendor/lib64/egl/libGLESv1_CM_POWERVR_ROGUE.so:system/vendor/lib64/egl/libGLESv1_CM_POWERVR_ROGUE.so \
     vendor/rockchip/common/gpu/libG6110/vendor/lib64/egl/libGLESv2_POWERVR_ROGUE.so:system/vendor/lib64/egl/libGLESv2_POWERVR_ROGUE.so \
     vendor/rockchip/common/gpu/libG6110/vendor/lib64/hw/gralloc.rk3368.so:system/vendor/lib64/hw/gralloc.rk3368.so \
-    vendor/rockchip/common/gpu/libG6110/vendor/lib64/hw/hwcomposer.rk3368.so:system/vendor/lib64/hw/hwcomposer.rk3368.so \
     vendor/rockchip/common/gpu/libG6110/vendor/lib64/libcreatesurface.so:system/vendor/lib64/libcreatesurface.so \
     vendor/rockchip/common/gpu/libG6110/vendor/lib64/libglslcompiler.so:system/vendor/lib64/libglslcompiler.so \
     vendor/rockchip/common/gpu/libG6110/vendor/lib64/libIMGegl.so:system/vendor/lib64/libIMGegl.so \
