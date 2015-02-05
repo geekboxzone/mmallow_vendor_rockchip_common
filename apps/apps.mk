ef477f82115772021cff3547b86a85164be13644
ljh <ljh@rock-chips.com>
2015-02-05 15:39:30 +0800

    Add Rk3grVideoPlayer.apk and Rk3grExplorer.apk

--- apps/apps.mk
+++ apps/apps.mk
@@ -4,16 +4,28 @@ PRODUCT_PACKAGES += \
     RkApkinstaller  \
     RkExpe          \
     RKUpdateService \
-    RkExplorer      \
     RkUserExperienceService \
     GoogleEmail
 
-ifneq ($(strip $(TARGET_BOARD_PLATFORM)), rk312x)
-PRODUCT_PACKAGES += \
-    Rk4kVideoPlayer
-else
+ifeq ($(strip $(TARGET_BOARD_PLATFORM)), rk312x)
 PRODUCT_PACKAGES += \
     RkVideoPlayer
+else
+ifeq ($(strip $(TARGET_BOARD_PLATFORM)), sofia3gr)
+PRODUCT_PACKAGES += \
+    Rk3grVideoPlayer
+else
+PRODUCT_PACKAGES += \
+    Rk4kVideoPlayer
+endif
+endif
+
+ifeq ($(strip $(TARGET_BOARD_PLATFORM)), sofia3gr)
+PRODUCT_PACKAGES += \
+    Rk3grExplorer
+else
+PRODUCT_PACKAGES += \
+    RkExplorer
 endif
 
 ifeq ($(strip $(BOARD_HAS_STRESSTEST_APP)), true)
