8b264759ba98cd1cb1d55ceccef344a68480221d
lly <lly@rock-chips.com>
2015-03-12 15:32:49 +0800

    add box apk
    (allapp,RKGameControlSettingV1,RKUpdateService,RkApkinstaller,
       RkBoxVideoPlayer,RkExplorer,RkMusic,Settings2,SimpleLauncher,eHomeMediaCenter_box,itvlauncher)

--- apps/RKUpdateService/Android.mk
+++ apps/RKUpdateService/Android.mk
@@ -3,6 +3,27 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(strip $(TARGET_BOARD_HARDWARE)), rk30board)
+ifeq ($(strip $(TARGET_BOARD_PLATFORM_PRODUCT)), box)
+include $(CLEAR_VARS)
+LOCAL_MODULE := RKUpdateService
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_BUILT_MODULE_STEM := package.apk
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+#LOCAL_PRIVILEGED_MODULE :=
+LOCAL_CERTIFICATE := PRESIGNED
+#LOCAL_OVERRIDES_PACKAGES := 
+LOCAL_SRC_FILES := $(LOCAL_MODULE)_box.apk
+#LOCAL_REQUIRED_MODULES :=
+ifeq ($(strip $(TARGET_ARCH)), arm)
+LOCAL_PREBUILT_JNI_LIBS := \
+    lib/arm/librockchip_update_jni.so
+else ifeq ($(strip $(TARGET_ARCH)), arm64)
+LOCAL_PREBUILT_JNI_LIBS := \
+    lib/arm64/librockchip_update_jni.so
+endif
+include $(BUILD_PREBUILT)
+else
 include $(CLEAR_VARS)
 LOCAL_MODULE := RKUpdateService
 LOCAL_MODULE_CLASS := APPS
@@ -22,6 +43,7 @@ LOCAL_PREBUILT_JNI_LIBS := \
     lib/arm64/librockchip_update_jni.so
 endif
 include $(BUILD_PREBUILT)
+endif
 endif
 
 ifeq ($(strip $(TARGET_BOARD_HARDWARE)), sofiaboard)
--- apps/apps.mk
+++ apps/apps.mk
@@ -11,12 +11,17 @@ ifeq ($(strip $(TARGET_BOARD_PLATFORM)), rk312x)
 PRODUCT_PACKAGES += \
     RkVideoPlayer
 else
+ifeq ($(strip $(TARGET_BOARD_PLATFORM_PRODUCT)), box)
+PRODUCT_PACKAGES += \
+    RkBoxVideoPlayer
+else
 ifeq ($(strip $(TARGET_BOARD_PLATFORM)), sofia3gr)
 PRODUCT_PACKAGES += \
     Rk3grVideoPlayer
 else
 PRODUCT_PACKAGES += \
     Rk4kVideoPlayer
+endif
 endif
 endif
 
@@ -40,4 +45,17 @@ PRODUCT_PACKAGES += \
     ituxd
 #PRODUCT_COPY_FILES += \
 #        $(LOCAL_PATH)/ituxd/lib/x86/libthermalJNI.so:system/lib/libthermalJNI.so 
+endif
+
+
+###########for box app ################
+ifeq ($(strip $(TARGET_BOARD_PLATFORM_PRODUCT)), box)
+PRODUCT_PACKAGES += \
+    AllApp	\
+    eHomeMediaCenter_box	\
+    ITVLauncher	\
+    RKGameControlSettingV1.0.1	\
+    RkMusic	\
+    Settings2	\
+    SimpleLauncher	
 endif
