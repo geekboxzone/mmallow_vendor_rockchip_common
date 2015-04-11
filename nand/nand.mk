19e05d1513cbfc900643d37e5feeee33d51446a9
hy <heyun@rock-chips.com>
2015-04-11 11:31:37 +0800

    rk3368: add drmboot.ko for arm64 architecture

--- nand/nand.mk
+++ nand/nand.mk
@@ -1,3 +1,4 @@
+
 ifeq ($(strip $(TARGET_ARCH)), arm)
 ifeq ($(strip $(TARGET_BOARD_PLATFORM)), rk3036)
 PRODUCT_COPY_FILES += \
@@ -6,7 +7,11 @@ else
 PRODUCT_COPY_FILES += \
     vendor/rockchip/common/nand/modules/arm/rk30xxnand_ko.ko.3.10.0:root/rk30xxnand_ko.ko.3.10.0
 endif
-
 PRODUCT_COPY_FILES += \
     vendor/rockchip/common/nand/modules/arm/drmboot.ko:root/drmboot.ko
+endif
+
+ifeq ($(strip $(TARGET_ARCH)), arm64)
+PRODUCT_COPY_FILES += \
+    vendor/rockchip/common/nand/modules/arm64/drmboot.ko:root/drmboot.ko
 endif
