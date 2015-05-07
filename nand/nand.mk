e5f459ee138deb2adff5043bf69a73ce4f59f465
shen zhi cheng <szc@rock-chips.com>
2015-05-07 14:56:08 +0800

    更新nand ko

--- nand/nand.mk
+++ nand/nand.mk
@@ -4,8 +4,13 @@ ifeq ($(strip $(TARGET_BOARD_PLATFORM)), rk3036)
 PRODUCT_COPY_FILES += \
     vendor/rockchip/common/nand/modules/arm/rk3036/rk30xxnand_ko.ko.3.10.0:root/rk30xxnand_ko.ko.3.10.0
 else
+ifeq ($(strip $(TARGET_BOARD_PLATFORM)), rk3188)
+PRODUCT_COPY_FILES += \
+    vendor/rockchip/common/nand/modules/arm/rk3188/rk30xxnand_ko.ko:root/rk30xxnand_ko.ko
+else
 PRODUCT_COPY_FILES += \
     vendor/rockchip/common/nand/modules/arm/rk30xxnand_ko.ko.3.10.0:root/rk30xxnand_ko.ko.3.10.0
+endif
 endif
 PRODUCT_COPY_FILES += \
     vendor/rockchip/common/nand/modules/arm/drmboot.ko:root/drmboot.ko
