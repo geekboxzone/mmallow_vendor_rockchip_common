667c65aea2be546d69b10bf4e1626522e6bf40ea
zyh <zyh@rock-chips.com>
2016-08-23 15:59:48 +0800

    disable mpp for 3288 cts,please open vpu hevc iommu in rk3288.dtsi
    Change-Id: Icdb2e65a62b396db4b7f75301e529d4e0549fdb3

--- vpu/Android.mk
+++ vpu/Android.mk
@@ -118,7 +118,7 @@ endif
 
 # use new vpu framework mpp
 USE_MPP := false
-ifneq ($(filter rk3228 rk3229 rk322x rk3288 rk3366 rk3399, $(strip $(TARGET_BOARD_PLATFORM))), )
+ifneq ($(filter rk3228 rk3229 rk322x rk3366 rk3399, $(strip $(TARGET_BOARD_PLATFORM))), )
 USE_MPP := true
 endif 
 
